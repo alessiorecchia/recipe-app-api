65ccc1f23957e585a1b91658aec555c96c53feec
Alessio <recchia.alessio@yahoo.com>
2024-06-04 16:12:53 +0200

    Add tests and create_superuser method

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,12 +12,39 @@ class ModelTests(TestCase):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
-        name='test'
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
-            name=name
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new user"""
+
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expexted in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expexted)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email rises a Value error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
